fcf554963484a6fdd75ddd07c7505de9e92b71fd
Pau Fernández <pau.fernandez@upc.edu>
2017-10-16 16:11:32 +0200

    Ara ja podem editar el títol.

--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/EditTitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/EditTitleActivity.java
@@ -3,6 +3,7 @@ package edu.upc.eseiaat.pma.titleapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class EditTitleActivity extends AppCompatActivity {
@@ -14,9 +15,19 @@ public class EditTitleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_title);
 
+        // (II)
         Intent intent = getIntent();
         String title = intent.getStringExtra("title");
         edit_title = (EditText)findViewById(R.id.edit_title);
         edit_title.setText(title);
+    }
+
+    public void saveTitle(View view) {
+        // (III)
+        String new_title = edit_title.getText().toString();
+        Intent data = new Intent();
+        data.putExtra("title", new_title);
+        setResult(RESULT_OK, data);
+        finish();
     }
 }
--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
@@ -9,21 +9,35 @@ import android.widget.TextView;
 public class TitleActivity extends AppCompatActivity {
 
     private String title = "Aquest és el títol inicial...";
+    private TextView title_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
-        TextView title_text = (TextView)findViewById(R.id.title);
+        title_text = (TextView)findViewById(R.id.title);
         title_text.setText(title);
     }
 
     public void editTitle(View view) {
+        // (I)
         // 1. Crear un 'intent'
         Intent intent = new Intent(this, EditTitleActivity.class);
         // 2. Afegir paràmetres (dades extra) a la crida a l'activitat
         intent.putExtra("title", title);
         // 3. Passar l'intent a Android perquè obri l'activitat
-        startActivity(intent);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+        case 0:
+            // (IV)
+            if (resultCode == AppCompatActivity.RESULT_OK) {
+                title = data.getStringExtra("title");
+                title_text.setText(title);
+            }
+        }
     }
 }
